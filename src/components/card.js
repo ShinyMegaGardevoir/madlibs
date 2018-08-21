95e3c182923fdf3e75ee8f16584b3d83beb00977
unknown <sadliergirls@gmail.com>
2018-08-21 12:08:26 -0600

    Added the form and generating, working build.

--- src/components/card.js
+++ src/components/card.js
@@ -1,31 +1,90 @@
 import React, { Component } from 'react';
 
 import Input from './input';
+import Content from './content';
+
+const INITIAL_STATE = {
+	color: '',
+	pluralNoun: '',
+	adjectiveOne: '',
+	celebOne: '',
+	adjectiveTwo: '',
+	nounOne: '',
+	numberOne: '',
+	numberTwo: '',
+	nounTwo: '',
+	adjectiveThree: '',
+	celebTwo: '',
+	celebThree: '',
+	adjectiveFour: '',
+	nounThree: '',
+	celebFour: '',
+	adjectiveFive: '',
+	contentVisible: false
+}
 
 class Card extends Component {
 	constructor() {
-		super()
+        super()
 
-		this.state = {
-			color: '',
-			pluralNoun: '',
+        this.state = INITIAL_STATE;
+
+				this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+		handleInputChange(event) {
+			this.setState({ [event.target.name]: event.target.value })
+			console.log(this.state);
 		}
 
-		this.handleInputChange = this.handleInputChange.bind(this);
-	}
+		handleFormSubmit(event) {
+			event.preventDefault()
+			if(this.state.contentVisible) {
+				this.setState(INITIAL_STATE)
+			} else {
+				this.setState({ contentVisible: true })
+			}
+		}
 
-	handleInputChange(event) {
-		this.setState({ color: event.target.value })
-		console.log(this.state.color);
-	}
+		render() {
 
-	render() {
+			const inputData = [
+					{title: 'Color', state: this.state.color, name: 'color'},
+					{title: 'Plural Noun', state: this.state.pluralNoun, name: 'pluralNoun'},
+					{title: 'Adjective', state: this.state.adjectiveOne, name: 'adjectiveOne'},
+					{title: 'Celebrity', state: this.state.celebOne, name: 'celebOne'},
+
+					{title: 'Adjective', state: this.state.adjectiveTwo, name: 'adjectiveTwo'},
+					{title: 'Noun', state: this.state.nounOne, name: 'nounOne'},
+					{title: 'Number', state: this.state.numberOne, name: 'numberOne'},
+					{title: 'Number', state: this.state.numberTwo, name: 'numberTwo'},
+
+					{title: 'Noun', state: this.state.nounTwo, name: 'nounTwo'},
+					{title: 'Adjective', state: this.state.adjectiveThree, name: 'adjectiveThree'},
+					{title: 'Celebrity', state: this.state.celebTwo, name: 'celebTwo'},
+					{title: 'Celebrity', state: this.state.celebThree, name: 'celebThree'},
+
+					{title: 'Adjective', state: this.state.adjectiveFour, name: 'adjectiveFour'},
+					{title: 'Noun', state: this.state.nounThree, name: 'nounThree'},
+					{title: 'Celebrity', state: this.state.celebFour, name: 'celebFour'},
+					{title: 'Adjective', state: this.state.adjectiveFive, name: 'adjectiveFive'}
+			]
+
+
 		return (
-			<div className = "card">
-			<h1>{this.state.color}</h1>
-			{ Input("Color", this.state.color, this.handleInputChange) }
-			{ Input("Plural Noun", this.state.pluralNoun, this.handleInputChange) }
+			<form onSubmit={this.handleFormSubmit} className = "card">
+			<div className = "card__inputs">
+			{
+				inputData.map(data => Input( (data), this.handleInputChange ))
+			}
 			</div>
+				<button type ="submit">{!this.state.contentVisible ? 'Generate' : 'Clear Form'}</button>
+				{
+					this.state.contentVisible ? <Content data = {this.state}/> : <div></div>
+				}
+			</form>
+
 			)
 	}
 }
